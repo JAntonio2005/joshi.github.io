c880f9d8040a0c28c13af2ec1af2ba1418e081e0
JAntonio2005 <joseantonioortihernandez777@gmail.com>
2025-02-26 15:44:11 -0600

    Subida del primer header codebase#1

---
+++ src/app/work-experince/work-experince.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-work-experince',
+  templateUrl: './work-experince.component.html',
+  styleUrl: './work-experince.component.css'
+})
+export class WorkExperinceComponent {
+
+}
